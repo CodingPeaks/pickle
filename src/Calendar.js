bf1ed2abb8b6051b7b3394c5e3ce414000c60238
MarcoNardone <mnardone@mrs-radar.it>
2023-04-14 22:13:01 +0200

    Month/year selection and basic clock UI
    Clicking the arrows now cycle between months and increment/decrement the year.
    Clicking the clock icon now flips the calendar and shows a basic time picker.
    
    Next TODOs:
    - Manage the time selection with buttons and scrollwheel
    - Overall design sucks, will try to improve it
    - Code cleanup

--- src/Calendar.js
+++ src/Calendar.js
@@ -1,48 +1,94 @@
 import './Calendar.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowLeft, faArrowRight, faClock } from '@fortawesome/free-solid-svg-icons'
+import { useEffect, useState } from 'react';
+const date = new Date();
 
 function Calendar() {
 
+    const [selectedYear, setSelectedYear] = useState(date.getFullYear())
+    const [selectedMonth, setSelectedMonth] = useState(date.getMonth() + 1)
+    const [daysInMonth, setDaysInMonth] = useState(getDaysInMonth(selectedYear, selectedMonth))
+    const [showClock, setShowClock] = useState(false)
+
+    useEffect(() => {
+        console.log("Month or year changed!");
+        setDaysInMonth(getDaysInMonth(selectedYear, selectedMonth));
+    }, [selectedYear, selectedMonth]);
+
     function getDaysInMonth(year, month) {
         return new Date(year, month, 0).getDate();
+    }
+
+    function handleMonthChange(direction) {
+        if (direction) {
+            console.log("Month forward")
+            if (selectedMonth === 12) {
+                setSelectedYear(selectedYear + 1);
+                setSelectedMonth(1);
+            } else {
+                setSelectedMonth(selectedMonth + 1);
+            }
+        } else {
+            console.log("Month backward")
+            if (selectedMonth === 1) {
+                setSelectedYear(selectedYear - 1);
+                setSelectedMonth(12);
+            } else {
+                setSelectedMonth(selectedMonth - 1);
+            }
+        }
+    }
+
+    function toggleClock() {
+        console.log("Showing clock =>", showClock)
+        setShowClock(!showClock);
     }
 
     const monthNames = ["January", "February", "March", "April", "May", "June",
         "July", "August", "September", "October", "November", "December"
     ];
 
-    const date = new Date();
-    const currentYear = date.getFullYear();
-    const currentMonth = date.getMonth() + 1;
-    const daysInCurrentMonth = getDaysInMonth(currentYear, currentMonth);
-    console.log(daysInCurrentMonth);
 
-    const rows = [];
-    for (let i = 1; i <= daysInCurrentMonth; i++) {
-        rows.push(i);
-    }
 
     return (
         <div className="Calendar-container">
             <div className='Calendar-header'>
 
-                <FontAwesomeIcon className="Calendar-arrow-ctrl" icon={faArrowLeft} />
-                <div className='Calendar-month-year'>
-                    {monthNames[currentMonth - 1] + " " + currentYear}
+                <FontAwesomeIcon className="Calendar-arrow-ctrl" icon={faArrowLeft} onClick={() => handleMonthChange(0)} />
+                <div className='Calendar-month-year' >
+                    {monthNames[selectedMonth - 1] + " " + selectedYear}
                 </div>
-                <FontAwesomeIcon className="Calendar-arrow-ctrl" icon={faArrowRight} />
+                <FontAwesomeIcon className="Calendar-arrow-ctrl" icon={faArrowRight} onClick={() => handleMonthChange(1)} />
 
                 <div className="Calendar-clock-ctrl">
-                    <FontAwesomeIcon className="Calendar-arrow-ctrl" icon={faClock} />
+                    <FontAwesomeIcon className={"Calendar-arrow-ctrl " + [showClock && "disabled"]} icon={faClock} onClick={toggleClock} />
                 </div>
 
             </div>
             <div className='Calendar-body'>
-                {
-                    rows.map(function (row, i) {
-                        return <div className='Calendar-day'>{row}</div>
-                    })}
+                {showClock ? Array(daysInMonth).fill(1).map((el, i) =>
+                    <div className='Calendar-day'>{i}</div>
+                ) : (
+                    <div className='Clock-selector'>
+
+                        <div className='Clock-hour-selector'>
+                            <div className='Clock-inc-button'>+</div>
+                            <div id='Clock-hour'>15</div>
+                            <div className='Clock-dec-button'>-</div>
+                        </div>
+
+                        <div className='Clock-two-dots'>:</div>
+
+                        <div className='Clock-minute-selector'>
+                            <div className='Clock-inc-button'>+</div>
+                            <div id='Clock-minute'>30</div>
+                            <div className='Clock-dec-button'>-</div>
+                        </div>
+
+                    </div>
+                )
+                }
             </div>
         </div>
     );
