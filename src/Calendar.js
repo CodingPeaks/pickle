f7d9a0b36cee855a90ec36fbd7626080b0e2b6d3
MarcoNardone <mnardone@mrs-radar.it>
2023-04-15 20:29:20 +0200

    Hour/minute controls, scrollwheel implementation, fixes
    Clicking the clock + and - buttons and scrolling now increment/decrement the hours/minutes.
    Basic fixes
    
    Next TODOs:
    - Overall design sucks, will try to improve it (still)
    - Code cleanup (STILL!!!!)

--- src/Calendar.js
+++ src/Calendar.js
@@ -10,6 +10,8 @@ function Calendar() {
     const [selectedMonth, setSelectedMonth] = useState(date.getMonth() + 1)
     const [daysInMonth, setDaysInMonth] = useState(getDaysInMonth(selectedYear, selectedMonth))
     const [showClock, setShowClock] = useState(false)
+    const [selectedHour, setSelectedHour] = useState(date.getHours())
+    const [selectedMinute, setSelectedMinute] = useState(date.getMinutes())
 
     useEffect(() => {
         console.log("Month or year changed!");
@@ -21,29 +23,86 @@ function Calendar() {
     }
 
     function handleMonthChange(direction) {
-        if (direction) {
-            console.log("Month forward")
-            if (selectedMonth === 12) {
-                setSelectedYear(selectedYear + 1);
-                setSelectedMonth(1);
+        if (!showClock) {
+            if (direction) {
+                console.log("Month forward")
+                if (selectedMonth === 12) {
+                    setSelectedYear(selectedYear + 1);
+                    setSelectedMonth(1);
+                } else {
+                    setSelectedMonth(selectedMonth + 1);
+                }
             } else {
-                setSelectedMonth(selectedMonth + 1);
+                console.log("Month backward")
+                if (selectedMonth === 1) {
+                    setSelectedYear(selectedYear - 1);
+                    setSelectedMonth(12);
+                } else {
+                    setSelectedMonth(selectedMonth - 1);
+                }
+            }
+        }
+    }
+
+
+    function handleHourChange(direction) {
+        if (direction) {
+            console.log("Hour forward")
+            if (selectedHour === 23) {
+                setSelectedHour(0);
+            } else {
+                setSelectedHour(selectedHour + 1);
             }
         } else {
-            console.log("Month backward")
-            if (selectedMonth === 1) {
-                setSelectedYear(selectedYear - 1);
-                setSelectedMonth(12);
+            console.log("Hour backward")
+            if (selectedHour === 0) {
+                setSelectedHour(23);
             } else {
-                setSelectedMonth(selectedMonth - 1);
+                setSelectedHour(selectedHour - 1);
+            }
+        }
+    }
+
+    function handleMinuteChange(direction) {
+        if (direction) {
+            console.log("Minute forward")
+            if (selectedMinute === 59) {
+                setSelectedMinute(0);
+            } else {
+                setSelectedMinute(selectedMinute + 1);
+            }
+        } else {
+            console.log("Minute backward")
+            if (selectedMinute === 0) {
+                setSelectedMinute(59);
+            } else {
+                setSelectedMinute(selectedMinute - 1);
+            }
+        }
+    }
+
+    function handleWheel(event) {
+        if (event.nativeEvent.wheelDelta > 0) {
+            console.log(event.target.id);
+            if (event.target.id == "Clock-hour") {
+                handleHourChange(1);
+            } else if (event.target.id == "Clock-minute") {
+                handleMinuteChange(1);
+            }
+        } else {
+            if (event.target.id == "Clock-hour") {
+                handleHourChange(0);
+            } else if (event.target.id == "Clock-minute") {
+                handleMinuteChange(0);
             }
         }
     }
 
     function toggleClock() {
-        console.log("Showing clock =>", showClock)
         setShowClock(!showClock);
     }
+
+    const zeroPad = (num) => String(num).padStart(2, '0')
 
     const monthNames = ["January", "February", "March", "April", "May", "June",
         "July", "August", "September", "October", "November", "December"
@@ -62,28 +121,28 @@ function Calendar() {
                 <FontAwesomeIcon className="Calendar-arrow-ctrl" icon={faArrowRight} onClick={() => handleMonthChange(1)} />
 
                 <div className="Calendar-clock-ctrl">
-                    <FontAwesomeIcon className={"Calendar-arrow-ctrl " + [showClock && "disabled"]} icon={faClock} onClick={toggleClock} />
+                    <FontAwesomeIcon className={"Calendar-arrow-ctrl"} icon={faClock} onClick={toggleClock} />
                 </div>
 
             </div>
             <div className='Calendar-body'>
-                {showClock ? Array(daysInMonth).fill(1).map((el, i) =>
-                    <div className='Calendar-day'>{i}</div>
+                {showClock == false ? Array(daysInMonth).fill(1).map((el, i) =>
+                    <div className='Calendar-day'>{i + 1}</div>
                 ) : (
                     <div className='Clock-selector'>
 
                         <div className='Clock-hour-selector'>
-                            <div className='Clock-inc-button'>+</div>
-                            <div id='Clock-hour'>15</div>
-                            <div className='Clock-dec-button'>-</div>
+                            <div className='Clock-inc-button' onClick={() => handleHourChange(1)}>+</div>
+                            <div id='Clock-hour' onWheel={handleWheel}>{zeroPad(selectedHour)}</div>
+                            <div className='Clock-dec-button' onClick={() => handleHourChange(0)}>-</div>
                         </div>
 
                         <div className='Clock-two-dots'>:</div>
 
                         <div className='Clock-minute-selector'>
-                            <div className='Clock-inc-button'>+</div>
-                            <div id='Clock-minute'>30</div>
-                            <div className='Clock-dec-button'>-</div>
+                            <div className='Clock-inc-button' onClick={() => handleMinuteChange(1)}>+</div>
+                            <div id='Clock-minute' onWheel={handleWheel}>{zeroPad(selectedMinute)}</div>
+                            <div className='Clock-dec-button' onClick={() => handleMinuteChange(0)}>-</div>
                         </div>
 
                     </div>
